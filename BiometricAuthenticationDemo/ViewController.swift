30169063f726e9ff254e91c3db97c1ecd7602798
Oldster <oldster189@gmail.com>
2017-12-13 23:41:03 +0700

    new app demo

--- BiometricAuthenticationDemo/ViewController.swift
+++ BiometricAuthenticationDemo/ViewController.swift
@@ -7,19 +7,56 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
-
+  
+  @IBOutlet weak var mUsernameTF: UITextField!
+  @IBOutlet weak var mPasswordTF: UITextField!
+  @IBOutlet weak var mLoginBtn: UIButton!
+  
+  let usernameKey = "demo"
+  let passwordKey = "1234"
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    mLoginBtn.layer.cornerRadius = 4
+    
+  } 
+  @IBAction func onClickVertifyTouchID(_ sender: Any) {
+    let context = LAContext()
+    if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: nil) {
+      context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "กรุณายืนยันตัวตนของท่าน", reply: { (isSuccessfuly, error) in
+        DispatchQueue.main.async {
+          if isSuccessfuly {
+            self.alertMessage("Status Vertify", "Authentication successfully!")
+          }else{
+            self.alertMessage("Status Vertify", "Authentication failed!")
+          }
+        }
+      })
+    }else{
+      alertMessage("Touch ID not available", "Your device is not configured for Touch ID.")
+    }
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  @IBAction func onClickLogin(_ sender: Any) {
+    if checkLogin(username: mUsernameTF.text!, password: mPasswordTF.text!) {
+      alertMessage("", "Login successfully!")
+    }else{
+      alertMessage("", "Username or password incorrect!")
+    }
   }
-
-
+  
+  func checkLogin(username: String, password: String) -> Bool {
+    return username == usernameKey && password == passwordKey
+  }
+  
+  func alertMessage(_ title:String, _ message:String){
+    let ac = UIAlertController(title: title, message: message, preferredStyle: .alert)
+    ac.addAction(UIAlertAction(title: "OK", style: .default))
+    self.present(ac, animated: true)
+  }
+  
 }
 
